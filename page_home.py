5eee170e42c91b681b8c0e21144479d8fdf6e351
lailybdriaa <lailybadriasrgr22@gmail.com>
2024-06-12 16:28:24 +0700

    Update

--- page_home.py
+++ page_home.py
@@ -21,9 +21,10 @@ def page_home():
             .university-logo {
                 display: block;
                 margin: 0 auto;
-                max-width: 50%;
+                max-width: 100%;
                 height: auto;
                 text-align: center;
+                margin-top: 100px;
             }
 
             .footer-text {
@@ -39,5 +40,5 @@ def page_home():
 
     st.markdown('<p class="header-text">Selamat datang di Aplikasi Prediksi Kelulusan Mahasiswa</p>', unsafe_allow_html=True)
     st.markdown('<p class="subheader-text">Universitas Darma Persada</p>', unsafe_allow_html=True)
-    st.image("Logo Unsada.png", caption="Logo Universitas Darma Persada")
+    st.markdown('<center> <img src="https://media.discordapp.net/attachments/955844341635633236/1250380933782310942/LogoUnsada.png?ex=666abb7d&is=666969fd&hm=0ec5b76162b5a5137b190ee4aef65d50201a9a79554b57fe07ff501c80fb9aed&=&format=webp&quality=lossless"> </center>', unsafe_allow_html=True)
     st.markdown('<p class="footer-text">© 2024 Universitas Darma Persada. All rights reserved.</p>', unsafe_allow_html=True)
