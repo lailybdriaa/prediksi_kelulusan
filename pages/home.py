a059e57a50800bc4fec92efdfbb954d534181a41
lailybdriaa <lailybadriasrgr22@gmail.com>
2024-06-12 15:51:58 +0700

    Drop file

--- pages/home.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def page_home():
-    st.header("Home")
-    st.write("Selamat datang di aplikasi Website Prediksi Kelulusan Mahasiswa")
-    st.image("Logo Unsada.png")
-    st.write("UNIVERSITAS DARMA PERSADA")
