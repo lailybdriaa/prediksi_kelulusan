9a3b1cb3ed5f1f491231edcd40b1dd6cdc9bd0b3
lailybdriaa <lailybadriasrgr22@gmail.com>
2024-06-12 16:08:57 +0700

    Update page_home.py

--- page_home.py
+++ page_home.py
@@ -1,7 +1,42 @@
 import streamlit as st
 
 def page_home():
-    st.header("Home")
-    st.write("Selamat datang di aplikasi Website Prediksi Kelulusan Mahasiswa")
-    st.image("Logo Unsada.png")
-    st.write("UNIVERSITAS DARMA PERSADA")
+    st.markdown(
+        """
+        <style>
+            .header-text {
+                font-size: 36px;
+                color: #333333;
+                text-align: center;
+                margin-bottom: 20px;
+            }
+
+            .subheader-text {
+                font-size: 24px;
+                color: #666666;
+                text-align: center;
+                margin-bottom: 30px;
+            }
+
+            .university-logo {
+                display: block;
+                margin: 0 auto;
+                max-width: 50%;
+                height: auto;
+            }
+
+            .footer-text {
+                font-size: 18px;
+                color: #999999;
+                text-align: center;
+                margin-top: 30px;
+            }
+        </style>
+        """
+        , unsafe_allow_html=True
+    )
+
+    st.markdown('<p class="header-text">Selamat datang di Aplikasi Prediksi Kelulusan Mahasiswa</p>', unsafe_allow_html=True)
+    st.markdown('<p class="subheader-text">Universitas Darma Persada</p>', unsafe_allow_html=True)
+    st.image("Logo_Unsada.png", caption="Logo Universitas Darma Persada", use_column_width=True)
+    st.markdown('<p class="footer-text">© 2024 Universitas Darma Persada. All rights reserved.</p>', unsafe_allow_html=True)
