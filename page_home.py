1ce5441206047655923fcb987a2d76a469640cc2
lailybdriaa <lailybadriasrgr22@gmail.com>
2024-06-12 15:56:12 +0700

    Test

---
+++ page_home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def page_home():
+    st.header("Home")
+    st.write("Selamat datang di aplikasi Website Prediksi Kelulusan Mahasiswa")
+    st.image("Logo Unsada.png")
+    st.write("UNIVERSITAS DARMA PERSADA")
--- page_prediksi.py
+++ page_prediksi.py
@@ -1,11 +1,7 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 import pandas as pd
-import sqlite3
 import joblib
-import page_home
-import page_prediksi
-import page_riwayat
+import sqlite3
 
 # Load model dan scaler
 best_model = joblib.load('models/best_model.pkl')
@@ -16,86 +12,6 @@ pca = joblib.load('models/pca.pkl')
 conn = sqlite3.connect('data/data_mahasiswa.db')
 c = conn.cursor()
 
-# Membuat tabel admin jika belum ada
-c.execute('''
-CREATE TABLE IF NOT EXISTS admin (
-    id INTEGER PRIMARY KEY,
-    nama TEXT,
-    username TEXT,
-    email TEXT,
-    password TEXT
-)
-''')
-
-# Membuat tabel mahasiswa jika belum ada
-c.execute('''
-CREATE TABLE IF NOT EXISTS mahasiswa (
-    id INTEGER PRIMARY KEY,
-    nama TEXT,
-    nim TEXT,
-    ips_semester_1 REAL,
-    ips_semester_2 REAL,
-    ips_semester_3 REAL,
-    ips_semester_4 REAL,
-    tagihan_semester_1 INTEGER,
-    tagihan_semester_2 INTEGER,
-    tagihan_semester_3 INTEGER,
-    tagihan_semester_4 INTEGER,
-    kehadiran INTEGER,
-    kelulusan TEXT
-)
-''')
-
-# Fungsi untuk mengubah halaman
-if 'page' not in st.session_state:
-    st.session_state.page = "Login"
-
-def set_page(page):
-    st.session_state.page = page
-    st.rerun()
-
-# Fungsi untuk memeriksa kredensial login
-def check_credentials(username, password):
-    c.execute('SELECT * FROM admin WHERE username = ? AND password = ?', (username, password))
-    return c.fetchone() is not None
-
-# Halaman Login
-def page_login():
-    st.header("Login Admin")
-    with st.form("login_form"):
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        login_button = st.form_submit_button(label="Login")
-
-    if login_button:
-        if check_credentials(username, password):
-            loginModal(username)
-        else:
-            st.error("Username atau password salah!")
-
-@st.experimental_dialog("Berhasil Login!")
-def loginModal(item):
-    st.write(f"Selamat Datang di Website Prediksi Kelulusan Mahasiswa {item}")
-    if st.button("Next"):
-        st.session_state.page = "Home"
-        st.rerun()
-
-@st.experimental_dialog("Apakah yakin ingin logout?")
-def logoutModal():
-    col1, col2, col4, col3, col5 = st.columns(5)
-    if col1.button("Logout"):
-        st.session_state.page = "Login"
-        st.rerun()
-    if col2.button("Cancel"):
-        st.rerun()
-
-def page_home():
-    st.header("Home")
-    st.write("Selamat datang di aplikasi Website Prediksi Kelulusan Mahasiswa")
-    st.image("Logo Unsada.png")
-    st.write("UNIVERSITAS DARMA PERSADA")
-
-# Halaman Form
 def page_form():
     st.header("Prediksi Kelulusan")
         
@@ -181,38 +97,3 @@ def page_form():
         
         conn.commit()
         st.success("Data berhasil disimpan ke dalam database!")
-
-# Halaman About
-def page_about():
-    st.header("Data Riwayat Mahasiswa")
-    c.execute('''SELECT * FROM mahasiswa''')
-    rows = c.fetchall()
-    if len(rows) > 0:
-        df_history = pd.DataFrame(rows, columns=['ID', 'Nama', 'NIM', 'IPS 1', 'IPS 2', 'IPS 3',
-                                                  'IPS 4', 'Tagihan 1', 'Tagihan 2',
-                                                  'Tagihan 3', 'Tagihan 4', 'Kehadiran (%)', 'Hasil'])
-        df_history = df_history.drop(columns=['ID'])
-        st.dataframe(df_history)
-    else:
-        st.write("Belum ada data yang tersimpan.")
-
-if st.session_state.page != "Login":
-    with st.sidebar:
-        selected = option_menu("Main Menu", ["Home", 'Prediksi', 'Riwayat','Logout'], 
-            icons=['house', 'gear','bi-clock-history', 'bi-box-arrow-right'], menu_icon="cast", default_index=1)
-        st.session_state.page = selected
-
-# Konten halaman
-if st.session_state.page == "Login":
-    page_login()
-elif st.session_state.page == "Home":
-    page_home()
-elif st.session_state.page == "Prediksi":
-    page_form()
-elif st.session_state.page == "Riwayat":
-    page_about()
-elif st.session_state.page == "Logout":
-    logoutModal()
-
-# Menutup koneksi database
-conn.close()
